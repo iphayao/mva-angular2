ca6192cdbbbfdb80fe61c061de0590efe60fc199
Phayao Boonon <phayaob@gmail.com>
2017-06-21 01:01:51 +0700

    Connecting the Blocks

--- ng2-new-app/app/main.ts
+++ ng2-new-app/app/main.ts
@@ -1,43 +1,26 @@
 import { bootstrap } from '@angular/platform-browser-dynamic';
 import { Component, OnInit, Injectable } from '@angular/core';
-import { HTTP_PROVIDERS, Http, Response } from '@angular/http';
-import { Observable } from 'rxjs/Observable';
-import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/toPromise';
 
 @Injectable()
 export class TaskService {
-    tasks;// = ["First Task", "Second Task", "Third Task"];
-
-    constructor(private _http: Http) { }
-                                                                      
-    getTask() {
-        var aPromise = this._http.get('/tasks.json')
-        .map((response: Response) => response.json().data)
-        .toPromise();
-
-        aPromise.then(tasksFromServer => this.tasks = tasksFromServer);
-    }
+    tasks = [1, 2, 3, 4, 5];
 }
 
 @Component({
     selector: 'tasks',
     providers: [TaskService],
     template: `<h4>This is the tasks components</h4>
-               {{ taskService.tasks | json }}
-               <ul>
-                    <li *ngFor="let task of taskService.tasks">
-                        {{ task.title }}
-                    </li>
-               </ul>`
+              <ul>
+                <li *ngFor="let task of taskService.tasks">
+                    {{ task }}
+                </li>
+              </ul>`
 })
 
 export class TasksComponent implements OnInit {
     constructor(public taskService: TaskService) {}
 
-    ngOnInit() {
-        this.taskService.getTask();
-    }
+    ngOnInit() {}
 
 }
 
@@ -54,4 +37,4 @@ export class AppComponent implements OnInit {
     ngOnInit() { }
 }
 
-bootstrap(AppComponent, [HTTP_PROVIDERS])
+bootstrap(AppComponent)
