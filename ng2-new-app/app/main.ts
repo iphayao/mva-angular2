5c861d507ad1fbb90da3c5e0817aa0faa9546e19
Phayao Boonon <phayaob@gmail.com>
2017-06-20 23:28:11 +0700

    Using Routing

--- ng2-new-app/app/main.ts
+++ ng2-new-app/app/main.ts
@@ -1,13 +1,25 @@
 import { bootstrap } from '@angular/platform-browser-dynamic';
 import { Component, OnInit, Injectable } from '@angular/core';
+import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS  } from '@angular/router-deprecated';
 
 @Injectable()
 export class TaskService {
     tasks = ["First Task", "Second Task", "Third Task"];
 
     update() {
-        
+
     }
+}
+
+@Component({
+    selector: 'other',
+    template: `<h4>Other component</h4>`
+})
+
+export class OtherComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
 }
 
 @Component({
@@ -31,10 +43,20 @@ export class TasksComponent implements OnInit {
 
 @Component({
     selector: 'my-app',
-    directives: [TasksComponent],
+    directives: [ROUTER_DIRECTIVES, TasksComponent],
+    providers: [ROUTER_PROVIDERS],
     template: `<h1>Hello World</h1>
-               <tasks></tasks>`
+               <a href="" [routerLink]="['Tasks']">Tasks</a>
+               <a href="" [routerLink]="['Other']">Other</a>
+               <a href="" [routerLink]="['Something']">Something</a>
+               <router-outlet></router-outlet>`
 })
+
+@RouteConfig([
+    {path: '/tasks', name: 'Tasks', component: TasksComponent, useAsDefault: true },
+    {path: '/other', name: 'Other', component: OtherComponent},
+    {path: '/somthing', name: 'Something', component: OtherComponent}
+])
 
 export class AppComponent implements OnInit {
     constructor() {}
